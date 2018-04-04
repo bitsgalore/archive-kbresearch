0b1f933240442a9b62eb071582230f84ab44c273
bitsgalore <johan.vanderknijff@kb.nl>
2018-04-04 16:11:46 +0200

    removed backup option

--- scrape.sh
+++ scrape.sh
@@ -4,7 +4,7 @@ url=http://blog.kbresearch.nl
 domains=blog.kbresearch.nl,wp.com,researchkb.files.wordpress.com,googleapis.com,gstatic.com
 
 logFile=wget.log
-wget --mirror --page-requisites --span-hosts --convert-links --backup-converted --adjust-extension -w 5 --random-wait --domains=$domains $url >>$logFile 2>&1
+wget --mirror --page-requisites --span-hosts --convert-links --adjust-extension -w 5 --random-wait --domains=$domains $url >>$logFile 2>&1
 
 # Power off the machine
 #poweroff
